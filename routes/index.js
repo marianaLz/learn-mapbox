ac48c96e3a2347eab2684e1b6e36dbc899eeb89b
mariana <marianaglp15@gmail.com>
2019-09-10 12:46:26 -0500

    form

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router  = express.Router();
+const Place = require('../models/Place')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -7,7 +8,29 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/map', (req, res, next) => {
-  res.render('map');
+  Place.find().then( places => {
+    console.log(places)
+    res.render('map', {places});
+  })
+});
+
+router.get("/new", (req, res) => {
+  res.render("new");
+});
+
+router.post("/new", (req, res) => {
+  let { lng, lat, name, address } = req.body;
+  let place = {
+    name,
+    address,
+    location: {
+      type: "Point",
+      coordinates: [lng, lat]
+    }
+  };
+  Place.create(place).then(() => {
+    res.redirect("/map");
+  });
 });
 
 module.exports = router;
